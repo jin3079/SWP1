615c7434bba529404d252abcb87488c2f5b8a460
jin3079 <hajinseo@kookmin.ac.kr>
2020-06-27 18:28:08 +0900

    issue #5 change calc.py (2)

--- calc/calc.py
+++ calc/calc.py
@@ -5,12 +5,12 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	first_num = d.get('first_num', [''])[0]
 	second_num = d.get('second_num', [''])[0]
-	sum, mul = 0, 0
+	sum, mul = -1, -1 
 	if '' not in [first_num, second_num]:
 		first_num, second_num = int(first_num), int(second_num)
 		sum = first_num + second_num
 		mul = first_num * second_num
-	response_body = html % {'sum':sum-1, 'mul':mul-1}
+	response_body = html % {'sum':sum, 'mul':mul}
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
 		('Content-Length', str(len(response_body)))
--- calc/template.py
+++ calc/template.py
@@ -2,8 +2,8 @@ html = """
 <html>
 	<body>
 		<form action="">
-			first numb = <input type = "number" name = "first_num"> <br><br>
-			second numb = <input type = "number" name = "second_num">
+			first num = <input type = "number" name = "first_num"> <br><br>
+			second num = <input type = "number" name = "second_num">
 		    <input type="submit">
 		</form>
 		Sum = %(sum)d <br><br>
