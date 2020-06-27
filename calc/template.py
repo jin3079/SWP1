d5eba3dd606a496f65847c7b765bed221117ef32
jin3079 <hajinseo@kookmin.ac.kr>
2020-06-27 16:35:44 +0900

    issue #4 add calc/calc.conf,calc.py,template.py

---
+++ calc/template.py
@@ -0,0 +1,13 @@
+html = """
+<html>
+	<body>
+		<form action="">
+			first numb = <input type = "number" name = "first_num"> <br><br>
+			second numb = <input type = "number" name = "second_num">
+		    <input type="submit">
+		</form>
+		Sum = %(sum)d <br><br>
+		Mul = %(mul)d
+	</body>
+</html>
+"""
