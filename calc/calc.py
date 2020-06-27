8fe18ef86a72a406989b7208091557df59b7c438
jin3079 <hajinseo@kookmin.ac.kr>
2020-06-27 19:39:06 +0900

    issue #6 Make exception handler

--- calc/calc.py
+++ calc/calc.py
@@ -5,11 +5,14 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	first_num = d.get('first_num', [''])[0]
 	second_num = d.get('second_num', [''])[0]
-	sum, mul = -1, -1 
-	if '' not in [first_num, second_num]:
+	sum, mul = 'None', 'None' 
+	try:
 		first_num, second_num = int(first_num), int(second_num)
 		sum = first_num + second_num
 		mul = first_num * second_num
+	except ValueError:
+		sum = 'Error'
+		mul = 'Error'
 	response_body = html % {'sum':sum, 'mul':mul}
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
